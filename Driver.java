241d854efdd3777d7e9135c3ef25d24831cd571c
Jared Lucas <jaredcooldude@gmail.com>
2018-11-07 12:19:53 -0500

    Wow new edit

--- Driver.java
+++ Driver.java
@@ -3,11 +3,22 @@ import java.util.*;
 public class Driver{
   
    public static void main(String[] args){
+      
+      Scanner input = new Scanner(System.in);
     
-      //insert code here
-
-     int a, b;
-
+      System.out.println("How many integers do you want to try to sort?");
+      int numToSort = input.nextInt();
+      
+      System.out.println("What range of integers do you want to sort?");
+      int rangeToSort = input.nextInt();
+      
+      /*need to create an object for each sort that contains a time reported
+         
+         Something like BubbleSort bubblesort = new BubbleSort(the number of ints entered, range of ints entered)
+         
+         Output the times calculated in each sort class
+      */
+      
    }
   
 }
